454ece13695043bf7579f43a9d5c21de74d7a7b1
ihlee <leeinhwan7@dgist.ac.kr>
2023-01-08 02:03:40 +0900

    add: typescript & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { auth } from './fbase';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { useState } from 'react';
+import { auth } from './firebase';
+import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth';
+import React, { useState } from 'react';
 import Test from './Test';
 
 function App() {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState<User | null>(null);
 
   function handleGoogleLogin() {
     const provider = new GoogleAuthProvider(); // provider를 구글로 설정
     signInWithPopup(auth, provider) // popup을 이용한 signup
       .then((data) => {
         setUserData(data.user); // user data 설정
-        console.log(data) // console로 들어온 데이터 표시
+        console.log(data); // console로 들어온 데이터 표시
       })
       .catch((err) => {
         console.log(err);
---
+++ src/Test.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const Test = () => <span>Test</span>;
+export default Test;
